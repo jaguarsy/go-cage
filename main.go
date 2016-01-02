3ac311024fb96bdbb93495c8d6afa72c9a48b1f6
JohnnyCage <shangyu@cheyaoshi.com>
2016-01-03 00:36:10 +0800

    增加cok指令

--- main.go
+++ main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"fmt"
 	"github.com/bitly/go-simplejson"
+	"github.com/metakeule/fmtdate"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
+	"strings"
+	"time"
 )
 
 func main() {
@@ -25,16 +28,13 @@ func main() {
 		return
 	}
 
-	if len(args) > 3 {
-		fmt.Println("unused args", args[3:])
-	}
-
 	command := args[1]
-	target := args[2]
 
 	switch command {
 	case "search":
-		search(target)
+		search(args[2])
+	case "cok":
+		cok(args[2], args[3])
 	default:
 		fmt.Println("invalid command", command)
 	}
@@ -51,10 +51,25 @@ func search(target string) {
 	go get(url, out)
 
 	body := <-out
-	decodeJSON(body)
+	decodeSearchJSON(body)
 }
 
-func decodeJSON(body string) {
+func cok(sid string, name string) {
+	if sid == "" || name == "" {
+		panic("invalid command parameters")
+	}
+
+	url := "http://cok.icrazy.me/search.php"
+	data := fmt.Sprintf("sid=%s&name=%s", sid, name)
+	out := make(chan string)
+
+	go post(url, data, out)
+
+	body := <-out
+	decodeCokJSON(body)
+}
+
+func decodeSearchJSON(body string) {
 	js, err := simplejson.NewJson([]byte(body))
 	if err != nil {
 		panic("json format error")
@@ -90,6 +105,44 @@ func decodeJSON(body string) {
 	}
 }
 
+func decodeCokJSON(body string) {
+	items, err := simplejson.NewJson([]byte(body))
+	if err != nil {
+		panic("json format error")
+	}
+
+	index := 0
+
+	fmt.Printf("\r%9s\t%2s\t%10s\t%10s\t%12s\n", "x", "y", "name", "time", "sid")
+	fmt.Println("-------------------------------------------------------------")
+
+	for {
+		item := items.GetIndex(index)
+
+		xPosition := item.Get("x").MustInt()
+		yPosition := item.Get("y").MustInt()
+		name := item.Get("name").MustString()
+		date := item.Get("lasttime").MustInt64()
+		sid := item.Get("sid").MustInt()
+
+		if name == "" && xPosition == 0 && yPosition == 0 {
+			return
+		}
+
+		tm := time.Unix(date, 0)
+
+		fmt.Printf("%2v - %5d\t%3d\t%8s\t%10s\t%5d\n",
+			index+1,
+			xPosition,
+			yPosition,
+			name,
+			fmtdate.Format("YYYY-MM-DD hh:mm:ss", tm),
+			sid)
+
+		index++
+	}
+}
+
 func get(url string, out chan string) {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", url, nil)
@@ -110,6 +163,24 @@ func get(url string, out chan string) {
 	if err != nil {
 		log.Fatalln(err)
 		return
+	}
+
+	out <- string(body)
+}
+
+func post(url string, data string, out chan string) {
+	resp, err := http.Post(url,
+		"application/x-www-form-urlencoded",
+		strings.NewReader(data))
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
 	}
 
 	out <- string(body)
